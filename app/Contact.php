004f3233b3b553bb6bf587d2575855a50b919eae
TopOneExpert <topOneExpert1121@protonmail.com>
2020-12-16 08:27:13 -0800

    fix error

--- app/Contact.php
+++ app/Contact.php
@@ -9,13 +9,28 @@ class Contact extends Model
     //
     protected $fillable = [
         'user_id',
-        'contact_user_id',
-        'email',        
+        'name',
+        'email',
+        'email_val1',
+        'email2',
+        'email_val2',
+        'phone_number1',
+        'phone_voice1',
+        'phone_sms1',
+        'phone_whatsapp1',
+        'phone_number2',
+        'phone_voice2',
+        'phone_sms2',
+        'phone_whatsapp2',
+        'phone_number3',
+        'phone_voice3',
+        'phone_sms3',
+        'phone_whatsapp3',
         'del_flag',
     ];
 
-    public function contact()
-    {
-        return $this->hasOne(User::class, 'id', 'contact_user_id');
-    }
+    // public function contact()
+    // {
+    //     return $this->hasOne(User::class, 'id', 'user_id');
+    // }
 }
